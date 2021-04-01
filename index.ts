c3da8c481a599ef89bb122f61762f0bd3ff52e41
Anwesh43 <anweshthecool0@gmail.com>
2021-04-01 06:53:23 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,44 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBounceBallInLine(context : CanvasRenderingContext2D, scale : number) {
+        const gap : number = w / (2 * parts + 1)
+        const r : number = gap / 2 
+        for (var j = 0; j < parts; j++) {
+            const sj : number = ScaleUtil.divideScale(scale, j, parts)
+            const sfj : number = ScaleUtil.sinify(sj)
+            const sfj1 : number = ScaleUtil.divideScale(sfj, 0, miniParts)
+            const sfj2 : number = ScaleUtil.divideScale(sfj, 1, miniParts)
+            const sfj3 : number = ScaleUtil.divideScale(sfj, 2, miniParts)
+            context.save()
+            context.translate(gap * (2 * j + 1), 0)
+            DrawingUtil.drawCircle(context, 0, r + (h - 2 * r) * sfj3, r * sfj1)
+            DrawingUtil.drawLine(context, -r * sfj1, h - r, r * sfj1, h - r)
+            context.restore()
+        }
+    }
+
+    static drawBBLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBounceBallInLine(context, scale)    
+    }
+}
